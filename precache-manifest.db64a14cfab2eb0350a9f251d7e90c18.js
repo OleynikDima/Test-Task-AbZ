5c6e017e91b2107c2f4cfe961323dcca6cf7ea8b
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-09-18 11:03:52 +0300

    Updates

---
+++ precache-manifest.db64a14cfab2eb0350a9f251d7e90c18.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c84f1fa4ac8055c698491f618a6b32f",
+    "url": "/Test-Task-AbZ/index.html"
+  },
+  {
+    "revision": "fbec19461d773ffe16d8",
+    "url": "/Test-Task-AbZ/static/css/main.5a0b65fe.chunk.css"
+  },
+  {
+    "revision": "d0283505df48e39276e0",
+    "url": "/Test-Task-AbZ/static/js/2.558b6254.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Test-Task-AbZ/static/js/2.558b6254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbec19461d773ffe16d8",
+    "url": "/Test-Task-AbZ/static/js/main.ab74c4e0.chunk.js"
+  },
+  {
+    "revision": "ebfdd5a492faa543dcb7",
+    "url": "/Test-Task-AbZ/static/js/runtime-main.16f93209.js"
+  },
+  {
+    "revision": "8a6a2a8962d1c2ac607e6f4c058b9b7b",
+    "url": "/Test-Task-AbZ/static/media/Layer_10.8a6a2a89.png"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/Test-Task-AbZ/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "e846ff46e5dfbd83cbc6ce0a1a193b20",
+    "url": "/Test-Task-AbZ/static/media/adult-apple-device-business-code-340152.e846ff46.png"
+  },
+  {
+    "revision": "e7d81efc8c9cfee4731bc38aeafc7db4",
+    "url": "/Test-Task-AbZ/static/media/dehaze-black-18dp.e7d81efc.svg"
+  }
+]);
